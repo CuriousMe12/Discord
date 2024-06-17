b0c5d9809da9594935a1612fef83489d1c27f8fa
vishalpandey4567 <vishalpandey8998@gmail.com>
2024-06-18 01:04:54 +0530

    Added shadcn button

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/** Why we require this util??
+ *  To add conditional CSS
+ *  by default -> i want bg-blue-500
+ *  state changes & true -> I want bg-red-500
+ *
+ *  This kind of conditional rendering of classes is handled by this inbuild utils
+ * */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
