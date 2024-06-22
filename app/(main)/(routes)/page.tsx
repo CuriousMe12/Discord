780144b93196d42919f56dcb65a99c1ddf34453d
vishalpandey4567 <vishalpandey8998@gmail.com>
2024-06-22 18:30:55 +0530

    Dark mode complete

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,8 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-4 w-40 p-4">
-      This is protected route
+    <div className="flex flex-col gap-y-4 w-40 p-4 ">
+      <h2 className="text-black dark:text-white">This is protected route</h2>
       <UserButton afterSignOutUrl="/" />
       <ModeToggle />
     </div>
